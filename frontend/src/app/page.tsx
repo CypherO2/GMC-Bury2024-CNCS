7385daafac7bb07dd926191f4d928f4e5f4cca81
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2024-03-14 10:15:16 +0000

    Create worlde style keyboard

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,95 +1,51 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { Button } from "@primer/react";
+import { SidebarExpandIcon } from '@primer/octicons-react'
+import {Box} from '@primer/react'
+
+let buttonStyle ={
+  padding: 5,
+};
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <main>
+      <Box sx={{borderRadius: 2, borderWidth: 1, borderStyle: 'solid', borderColor: 'border.default', p: 3, width: '70%'}}>
+        <Box sx={{display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr', gridGap: 3}}>
+          <Button sx={buttonStyle}>Q</Button>
+          <Button sx={buttonStyle}>W</Button>
+          <Button sx={buttonStyle}>E</Button>
+          <Button sx={buttonStyle}>R</Button>
+          <Button sx={buttonStyle}>T</Button>
+          <Button sx={buttonStyle}>Y</Button>
+          <Button sx={buttonStyle}>U</Button>
+          <Button sx={buttonStyle}>I</Button>
+          <Button sx={buttonStyle}>O</Button>
+          <Button sx={buttonStyle}>P</Button>
+        </Box>
+        <Box sx={{display: 'grid', gridTemplateColumns: '0.5fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr', gridGap: 3, paddingTop: 3}}>
+          <span></span>
+          <Button sx={buttonStyle}>A</Button>
+          <Button sx={buttonStyle}>S</Button>
+          <Button sx={buttonStyle}>D</Button>
+          <Button sx={buttonStyle}>F</Button>
+          <Button sx={buttonStyle}>G</Button>
+          <Button sx={buttonStyle}>H</Button>
+          <Button sx={buttonStyle}>J</Button>
+          <Button sx={buttonStyle}>K</Button>
+          <Button sx={buttonStyle}>L</Button>
+        </Box>
+        <Box sx={{display: 'grid', gridTemplateColumns: '1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr', gridGap: 3, paddingTop: 3}}>
+          <Button sx={buttonStyle}>ENTER</Button>
+          <Button sx={buttonStyle}>Z</Button>
+          <Button sx={buttonStyle}>X</Button>
+          <Button sx={buttonStyle}>C</Button>
+          <Button sx={buttonStyle}>V</Button>
+          <Button sx={buttonStyle}>B</Button>
+          <Button sx={buttonStyle}>N</Button>
+          <Button sx={buttonStyle}>M</Button>
+          <Button sx={buttonStyle}><SidebarExpandIcon/></Button>
+        </Box>
+      </Box>
     </main>
   );
 }
